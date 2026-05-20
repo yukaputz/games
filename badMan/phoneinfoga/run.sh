34efc7b3ce75cf8bd9c4c000bfda60f989210fc6
yukaputz <yukaputz@gmail.com>
2026-05-20 13:45:26 -0400

    Update project files

--- badMan/phoneinfoga/run.sh
+++ badMan/phoneinfoga/run.sh
@@ -6,3 +6,9 @@ docker run --rm osint-phoneinfoga scan -n +17035878023
 
 # Web UI
 docker run --rm -it -p 5000:5000 osint-phoneinfoga serve
+
+docker run -d \
+  --name phoneinfoga \
+  -p 8082:8082 \
+  -e NUMVERIFY_API_KEY="dc2cc0f92aed9b24ef04bf4fb8ccae" \
+  sundowndev/phoneinfoga serve -p 8082
